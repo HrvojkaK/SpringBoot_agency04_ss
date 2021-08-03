3e4ec63a47e28127cc146a900fa4f08c2e6c10fb
Hrvojka Krcelic <hrvojka.krcelic@gmail.com>
2021-08-03 04:18:25 +0100

    week 2 part 3

--- src/main/java/com/agency04/sbss/pizza/PizzaApp.java
+++ src/main/java/com/agency04/sbss/pizza/PizzaApp.java
@@ -2,12 +2,17 @@ package com.agency04.sbss.pizza;
 import com.agency04.sbss.pizza.pizzamodels.*;
 import com.agency04.sbss.pizza.pizzeriaservice.*;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
+@SpringBootApplication
 public class PizzaApp {
 
     public static void main(String[] args) {
+
+        SpringApplication.run(PizzaApp.class, args);
 
         //read the spring config class
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(PizzaConfig.class);
