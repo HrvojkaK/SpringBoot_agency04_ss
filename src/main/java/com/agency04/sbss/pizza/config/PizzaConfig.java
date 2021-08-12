e8a46ceafa0893fd9b6048a03f9310baf763ef58
Hrvojka Krcelic <hrvojka.krcelic@gmail.com>
2021-08-12 13:21:44 +0100

    Week 3

--- src/main/java/com/agency04/sbss/pizza/config/PizzaConfig.java
+++ src/main/java/com/agency04/sbss/pizza/config/PizzaConfig.java
@@ -3,12 +3,9 @@ import com.agency04.sbss.pizza.pizzeriaservice.FirstPizzeria;
 import com.agency04.sbss.pizza.pizzeriaservice.PizzeriaService;
 import com.agency04.sbss.pizza.pizzeriaservice.SecondPizzeria;
 import org.springframework.context.annotation.*;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @Configuration
-//new 3
-@EnableWebMvc
-//w3: i dodam novi komponent scan package jer bum stavljala nove beanove u neki novi folder:
 @ComponentScan("com.agency04.sbss.pizza.pizzeria")
 @ComponentScan("com.agency04.sbss.pizza.pizzeriaservice")
 @PropertySource("classpath:application.properties")
